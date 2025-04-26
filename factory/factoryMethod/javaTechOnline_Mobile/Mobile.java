b73e603d3af65b5efd4df451b8809423621c5760
RahilTari <RahilTari@gmail.com>
2025-04-26 14:20:08 +0330

    Factory method design pattern implementation...

---
+++ factory/factoryMethod/javaTechOnline_Mobile/Mobile.java
@@ -0,0 +1,7 @@
+package tari.rahil.java.main.src.designPattern.factoryMethod.javaTechOnline_Mobile;
+//Product
+public interface Mobile {
+    void cost();
+    void pictureCapacity();
+    void batteryPower();
+}
---
+++ factory/factoryMethod/refactoring/guru_GUI_elements/buttonsAsProduct/Button.java
@@ -0,0 +1,6 @@
+package tari.rahil.java.main.src.designPattern.factoryMethod.refactoring.guru_GUI_elements.buttonsAsProduct;
+//Common product interface
+public interface Button {
+    void render();
+    void onClick();
+}
